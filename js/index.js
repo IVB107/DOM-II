c91e8895b9012302ea8af6de8a86cfe040215076
IVB107 <ianvb107@gmail.com>
2019-01-29 16:38:40 -0600

    Added animations to nav links

--- js/index.js
+++ js/index.js
@@ -3,7 +3,19 @@
 const header = document.querySelector("nav");
 window.addEventListener("load", e => {
     TweenMax.from(".logo-heading", 1, {y: -200, rotation: -90, ease:Bounce.easeOut});
-    TweenMax.staggerFrom(".nav-link", 3, {x: 1000, opacity: .5, rotation:1440, ease:Elastic.easeOut.config(1, 0.9), delay: 1}, 0.2);
+    TweenMax.staggerFrom(".nav-link", 2, {x: 1000, opacity: .5, rotation:1440, ease:Elastic.easeOut.config(1, 0.9), delay: .8}, 0.2);
+});
+
+const navLink = document.querySelectorAll("nav a");
+navLink.forEach(link => {
+    link.addEventListener("mouseover", e => {
+        TweenMax.to(link, .1, {scale: 1.2, color: 'orange', ease:Ease.easeOut});
+        // e.target.style.borderBottom = "2px solid black";
+    });
+    link.addEventListener("mouseout", e => {
+        TweenMax.to(link, .1, {scale: 1, color: 'black', ease:Ease.easeOut});
+        // e.target.style.borderBottom = "none";
+    });
 });
 
 
@@ -12,19 +24,23 @@ window.addEventListener("load", e => {
 
 
 
-
-
 // Methods to explore: 
-// * [ ] `mouseover`
 // * [ ] `keydown`
 // * [ ] `wheel`
 // * [ ] `drag / drop`
-// * [ ] `load`
 // * [ ] `focus`
 // * [ ] `resize`
 // * [ ] `scroll`
 // * [ ] `select`
 // * [ ] `dblclick`    
+
+
+// Methods Used:
+// * [ ] `load`
+// * [ ] `mouseover`
+// * [ ] `mouseout`
+
+
 
 
 // Ideas:
